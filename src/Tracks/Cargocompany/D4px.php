070f70a565bd881d9a72a63d8295c8592458a7d1
caiyu <928692509@qq.com>
2019-08-29 12:47:19 +0800

    万邦物流商轨迹接口对接

---
+++ src/Tracks/Cargocompany/D4px.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/20
+ * Time: 11:38
+ */
+
+namespace Burning\YibaiLogistics\Tracks\Cargocompany;
+
+
+class D4px
+{
+
+}
---
+++ src/Tracks/Cargocompany/OneWorldExpress.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/29
+ * Time: 11:05
+ */
+
+namespace Burning\YibaiLogistics\Tracks\Cargocompany;
+
+
+class OneWorldExpress
+{
+
+}
